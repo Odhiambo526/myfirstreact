24212eccc5ebc66ac9349e90b7ce23e8c9288c8e
Daniel Odhiambo <72065059+Odhiambo526@users.noreply.github.com>
2021-11-25 11:56:06 +0300

    Updates

---
+++ precache-manifest.308c94f5a80e4f696d4d87ae99cb2051.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "248b99cbbb4b65b0845d85326e081e95",
+    "url": "/myfirstreact/index.html"
+  },
+  {
+    "revision": "203ffb5ad3ebd2606e44",
+    "url": "/myfirstreact/static/css/main.2b87e097.chunk.css"
+  },
+  {
+    "revision": "4b5a0659b690fe5c44a5",
+    "url": "/myfirstreact/static/js/2.107f0c64.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/myfirstreact/static/js/2.107f0c64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "203ffb5ad3ebd2606e44",
+    "url": "/myfirstreact/static/js/main.be06fd15.chunk.js"
+  },
+  {
+    "revision": "39e66660c1c572565014",
+    "url": "/myfirstreact/static/js/runtime-main.bb6c22e3.js"
+  }
+]);
